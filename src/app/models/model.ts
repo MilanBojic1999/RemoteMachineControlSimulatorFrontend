dbde103405dcdbd86d831dc9a56f75d8f0277d48
milan boi <mbojic12@gmail.com>
2021-12-25 19:27:39 +0100

    Calling backend is ghood

---
+++ src/app/models/model.ts
@@ -0,0 +1,19 @@
+
+export interface Permission {
+  value:string;
+}
+
+export interface UserDTO {
+  email:string;
+  firstname:string;
+  lastname:string;
+  permissions: Permission[]
+}
+
+export interface UserFull {
+  email:string;
+  firstname:string;
+  lastname:string;
+  password:string;
+  permissions: Permission[]
+}
--- src/app/service/usero.service.ts
+++ src/app/service/usero.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {UserDTO} from "../models/model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,18 @@ export class UseroService {
   private readonly apiUrl = environment.postApi;
 
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) {
+    console.log("Dandalion line is connecting...")
+  }
+
+  getUsers(){
+    let jwt =  localStorage.getItem('jwt');
+    if(jwt == null){
+      throw new Error('Empty jwt token')
+    }
+    let path = this.apiUrl + '/show/all'
+    return this.httpClient.get<UserDTO>(path,{
+      headers: new HttpHeaders().set('Authorization',jwt)
+    }).subscribe();
+  }
 }
